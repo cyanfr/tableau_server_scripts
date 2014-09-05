4ceaf7d8c3e7be8c13e709192a1764d304ebda61
Meserole, Andrew <andrew.meserole@neustar.biz>
2014-09-05 09:54:55 -0400

    report_export.bat minor changes

--- report_export.bat
+++ report_export.bat
@@ -3,7 +3,7 @@
 :: ---------------------------------------------------------------------------------------
 :: The MIT License (MIT)
 :: 
-:: Copyright (c) 2014 Andrew Meserole
+:: Copyright (c) 2014 cyanfr @ github
 :: 
 :: Permission is hereby granted, free of charge, to any person obtaining a copy
 :: of this software and associated documentation files (the "Software"), to deal
